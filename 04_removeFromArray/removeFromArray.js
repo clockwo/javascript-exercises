b865224814e8cf3c444391f9b878f4d272076647
clockwo <39350729+clockwo@users.noreply.github.com>
2023-06-28 22:04:06 +0200

    feat: :sparkles: Make exercises from 01 to 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+// * First attempt
+// const removeFromArray = function (array, ...removeArgs) {
+//   for (const item of removeArgs) {
+//     for (let i = 0; i < array.length; i++) {
+//       if (item === array[i]) {
+//         array.splice(i, 1);
+//       }
+//     }
+//   }
+//   return array;
+// };
 
+// * Second attempt
+const removeFromArray = function (array, ...removeArgs) {
+  return array.filter((item) => !removeArgs.includes(item));
 };
 
 // Do not edit below this line
