c7733788aee8270a3deb2bcbbb8a3bd3b78b6723
clockwo <39350729+clockwo@users.noreply.github.com>
2023-06-28 22:02:56 +0200

    feat: :sparkles: Make exercises from 01 to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+  // Check if the numbers are positive integers
+  if (
+    startNum < 0 ||
+    endNum < 0 ||
+    !Number.isInteger(startNum) ||
+    !Number.isInteger(endNum)
+  ) {
+    return 'ERROR';
+  }
+  // Swap numbers if startNum is larger
+  if (startNum > endNum) {
+    const swapNumber = endNum;
+    endNum = startNum;
+    startNum = swapNumber;
+  }
 
+  // Calculate the sum of all numbers in the range
+  let rangeLength = endNum - startNum + 1;
+  let endPointsSum = startNum + endNum;
+  return (rangeLength * endPointsSum) / 2;
 };
 
 // Do not edit below this line
