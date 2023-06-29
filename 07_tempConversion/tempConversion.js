572d55e4390644879ca1d10d34133f4155e43c4c
clockwo <39350729+clockwo@users.noreply.github.com>
2023-06-29 11:35:04 +0200

    feat: :sparkles: Make from 06 to 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  return parseFloat(((5 / 9) * (temp - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  return parseFloat(((9 / 5) * temp + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
